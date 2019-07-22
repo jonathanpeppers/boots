fbd81687b5f5dda6373511a9c597bf53bdb302fb
Jonathan Peppers <jonathan.peppers@microsoft.com>
2019-07-22 16:08:35 -0500

    Skip test on Windows

--- Boots.Tests/BootstrapperTests.cs
+++ Boots.Tests/BootstrapperTests.cs
@@ -14,11 +14,19 @@ namespace Boots.Tests
 			boots.Logger = new TestWriter (output);
 		}
 
-		[SkippableFact]
+		[SkippableFact (Skip = "On AzDO it wants to close `vstest.console.exe`, skip for now.")]
 		public async Task ProjectSystemToolsVsix ()
 		{
 			Skip.IfNot (Helpers.IsWindows);
 			boots.Url = "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/VisualStudioProductTeam/vsextensions/ProjectSystemTools/1.0.1.1927902/vspackage";
+			await boots.Install ();
+		}
+
+		[SkippableFact]
+		public async Task ObjectiveSharpiePkg ()
+		{
+			Skip.IfNot (Helpers.IsMac);
+			boots.Url = "https://aka.ms/objective-sharpie";
 			await boots.Install ();
 		}
 	}
