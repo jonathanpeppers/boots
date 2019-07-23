2f2664937d5f77998cb8541bb2e97c8e35ec3d3e
Jonathan Peppers <jonathan.peppers@microsoft.com>
2019-07-23 13:39:06 -0500

    User-friendly help text

--- Boots/Program.cs
+++ Boots/Program.cs
@@ -9,14 +9,34 @@ namespace Boots
 	{
 		static async Task Main (string [] args)
 		{
+			Version ();
+			if (args.Length == 0 || args [0] == "--help") {
+				Help ();
+				return;
+			}
+
 			var cts = new CancellationTokenSource ();
 			Console.CancelKeyPress += (sender, e) => cts.Cancel ();
 
-			var boots = new Bootstrapper ();
-			if (args.Length > 0) {
-				boots.Url = args [0];
-			}
+			var boots = new Bootstrapper {
+				Url = args [0],
+			};
 			await boots.Install (cts.Token);
+		}
+
+		static void Version ()
+		{
+			var name = typeof (Program).Assembly.GetName ();
+			Console.WriteLine ($"boots {name.Version}");
+		}
+
+		static void Help ()
+		{
+			Console.WriteLine ();
+			Console.WriteLine ("usage:");
+			Console.WriteLine ("\tboots https://url/to/your/package");
+			Console.WriteLine ();
+			Console.WriteLine ("On Windows, a .vsix file is assumed and .pkg on OSX. Linux is not currently supported.");
 		}
 	}
 }
