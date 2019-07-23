6ad1b3c57ee778217cf011c898abbfdde4e0553b
Jonathan Peppers <jonathan.peppers@microsoft.com>
2019-07-23 16:05:31 -0500

    Update appcenter-pre-build.sh

--- samples/HelloForms.Android/appcenter-pre-build.sh
+++ samples/HelloForms.Android/appcenter-pre-build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
+
 boots https://download.mono-project.com/archive/6.0.0/macos-10-universal/MonoFramework-MDK-6.0.0.313.macos10.xamarin.universal.pkg
 boots https://aka.ms/xamarin-android-commercial-d16-2-macos
