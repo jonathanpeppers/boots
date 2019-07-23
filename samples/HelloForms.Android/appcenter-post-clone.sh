383c8af0c2217405aa15f8a3e99abdc1f537fa1f
Jonathan Peppers <jonathan.peppers@microsoft.com>
2019-07-23 16:46:17 -0500

    Update appcenter-post-clone.sh

--- samples/HelloForms.Android/appcenter-post-clone.sh
+++ samples/HelloForms.Android/appcenter-post-clone.sh
@@ -2,12 +2,11 @@
 
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
 
-dotnet --version
-dotnet --list-runtimes
 dotnet tool install --global boots --version 0.1.0.251-beta
 
 # Workaround instead of restarting shell
 export PATH="$PATH:~/.dotnet/tools"
+export DOTNET_ROOT="$(dirname "$(readlink "$(command -v dotnet)")")"
 
 boots https://download.mono-project.com/archive/6.0.0/macos-10-universal/MonoFramework-MDK-6.0.0.313.macos10.xamarin.universal.pkg
 boots https://aka.ms/xamarin-android-commercial-d16-2-macos
