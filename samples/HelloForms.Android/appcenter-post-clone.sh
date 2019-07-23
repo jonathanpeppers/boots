d39bcf5de1253bc579a77edb08ee1d61a957e7d1
Jonathan Peppers <jonathan.peppers@microsoft.com>
2019-07-23 15:30:38 -0500

    AppCenter custom script setup

---
+++ samples/HelloForms.Android/appcenter-post-clone.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+dotnet tool install --global boots --version 0.1.0.251-beta
+boots https://download.mono-project.com/archive/6.0.0/macos-10-universal/MonoFramework-MDK-6.0.0.313.macos10.xamarin.universal.pkg
+boots https://aka.ms/xamarin-android-commercial-d16-2-macos
