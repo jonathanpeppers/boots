d9d117f946c53264a91cfa1f6b7879a92946223c
Jonathan Peppers <jonathan.peppers@microsoft.com>
2019-07-24 11:29:32 -0500

    Cake Addin implementation (#7)
    I also consolidated many of the NuGet settings in Directory.Build.props

---
+++ Cake.Boots/BootsAddin.cs
@@ -0,0 +1,48 @@
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Boots.Core;
+using Cake.Core;
+using Cake.Core.Annotations;
+using Cake.Core.Diagnostics;
+
+namespace Cake.Boots
+{
+	public static class BootsAddin
+	{
+		[CakeMethodAlias]
+		public static async Task Boots (this ICakeContext context, string url)
+		{
+			var boots = new Bootstrapper {
+				Url = url,
+				Logger = new CakeWriter (context)
+			};
+
+			await boots.Install ();
+		}
+
+		class CakeWriter : TextWriter
+		{
+			const Verbosity verbosity = Verbosity.Normal;
+			const LogLevel level = LogLevel.Information;
+			readonly ICakeContext context;
+
+			public CakeWriter (ICakeContext context)
+			{
+				this.context = context;
+			}
+
+			public override Encoding Encoding => Encoding.Default;
+
+			public override void WriteLine (string value)
+			{
+				context.Log.Write (verbosity, level, value ?? "");
+			}
+
+			public override void WriteLine (string format, params object [] args)
+			{
+				context.Log.Write (verbosity, level, format, args);
+			}
+		}
+	}
+}
---
+++ Cake.Boots/build.cake
@@ -0,0 +1,20 @@
+var target = Argument("target", "Default");
+
+// Just import both
+#r "bin/Debug/netstandard2.0/Cake.Boots.dll"
+#r "bin/Release/netstandard2.0/Cake.Boots.dll"
+
+Task("Boots")
+    .Does(async () =>
+{
+    var url = IsRunningOnWindows() ?
+        "https://github.com/codecadwallader/codemaid/releases/download/v11.0/CodeMaid.v11.0.183.vsix" :
+        "https://aka.ms/objective-sharpie";
+
+    await Boots (url);
+});
+
+Task("Default")
+    .IsDependentOn("Boots");
+
+RunTarget(target);
