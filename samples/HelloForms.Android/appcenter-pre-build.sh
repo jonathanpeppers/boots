faf7bbd3659f2e875ce431757e0119859b2c83e7
Jonathan Peppers <jonathan.peppers@microsoft.com>
2019-09-26 11:27:11 -0500

    Get App Center working again
    It looks like the workaround for `$PATH` is no longer needed.

--- samples/HelloForms.Android/appcenter-pre-build.sh
+++ samples/HelloForms.Android/appcenter-pre-build.sh
@@ -4,11 +4,5 @@
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
 
 dotnet tool install --global boots
-
-# Workaround instead of restarting shell
-# see: https://github.com/dotnet/cli/issues/9114#issuecomment-494226139
-export PATH="$PATH:~/.dotnet/tools"
-export DOTNET_ROOT="$(dirname "$(readlink "$(command -v dotnet)")")"
-
 boots https://download.mono-project.com/archive/6.4.0/macos-10-universal/MonoFramework-MDK-6.4.0.189.macos10.xamarin.universal.pkg
 boots https://aka.ms/xamarin-android-commercial-d16-3-macos
