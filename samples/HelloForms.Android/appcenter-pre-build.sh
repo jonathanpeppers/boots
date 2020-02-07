bb3c63a560ec94db34c00988374dc9fe6cde7d0e
Jonathan Peppers <jonathan.peppers@gmail.com>
2020-02-07 14:07:21 -0600

    [build] bootstrap boots with boots 1.0.2 (#28)

--- samples/HelloForms.Android/appcenter-pre-build.sh
+++ samples/HelloForms.Android/appcenter-pre-build.sh
@@ -3,7 +3,7 @@
 
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
 
-dotnet tool install --global boots --version 1.0.2.407-pre
+dotnet tool install --global boots --version 1.0.2.421
 boots --preview Mono
 boots --preview XamarinAndroid
 boots --preview XamariniOS
