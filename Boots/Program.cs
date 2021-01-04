674d09b6e3c63aecb5f5adc19b58679d8e8696c8
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-01-04 08:51:34 -0600

    Bump to System.CommandLine 2.0.0-beta1.20574.7 (#56)

--- Boots/Program.cs
+++ Boots/Program.cs
@@ -64,9 +64,9 @@ namespace Boots
 
 		static string Validator (CommandResult result)
 		{
-			if (result.ValueForOption ("--url") == null &&
-				result.ValueForOption ("--stable") == null &&
-				result.ValueForOption ("--preview") == null) {
+			if (result.GetArgumentValueOrDefault ("--url") == null &&
+				result.GetArgumentValueOrDefault ("--stable") == null &&
+				result.GetArgumentValueOrDefault ("--preview") == null) {
 				return "At least one of --url, --stable, or --preview must be used";
 			}
 			return "";
