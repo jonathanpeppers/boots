jonathanpeppers/boots

commit 674d09b6e3c63aecb5f5adc19b58679d8e8696c8

Bump to System.CommandLine 2.0.0-beta1.20574.7 (#56)

Boots/Program.cs
@@ -64,9 +64,9 @@ namespace Boots
 
 		static string Validator (CommandResult result)
 		{
-			if (result.ValueForOption ("--url") == null &&
-				result.ValueForOption ("--stable") == null &&
-				result.ValueForOption ("--preview") == null) {
+			if (result.GetArgumentValueOrDefault ("--url") == null &&
+				result.GetArgumentValueOrDefault ("--stable") == null &&
+				result.GetArgumentValueOrDefault ("--preview") == null) {
 				return "At least one of --url, --stable, or --preview must be used";
 			}
 			return "";

Boots/Boots.csproj
@@ -14,7 +14,7 @@
   </PropertyGroup>
 
   <ItemGroup>
-    <PackageReference Include="System.CommandLine" Version="2.0.0-beta1.20071.2" />
+    <PackageReference Include="System.CommandLine" Version="2.0.0-beta1.20574.7" />
   </ItemGroup>
 
   <ItemGroup>

commit 45567deed9d0602ed64457baf6dea4ac9b85f29b

Fix `boots` command-line arguments
I noticed this on CI:

    > dotnet Boots/bin/$(Configuration)/netcoreapp3.1/Boots.dll --preview Xamarin.Android
    At least one of --url, --stable, or --preview must be used

What? `--preview` was passed???

This broke in 674d09b6e, when I used the wrong API for validating
incoming command-line options...

I will delist 1.0.3.551.

To prevent this in the future, I added `MainTests` that actually check
that the `static Task Main()` returns the proper output and works
correctly.

Boots/Program.cs
@@ -64,9 +64,9 @@ namespace Boots
 
 		static string Validator (CommandResult result)
 		{
-			if (result.GetArgumentValueOrDefault ("--url") == null &&
-				result.GetArgumentValueOrDefault ("--stable") == null &&
-				result.GetArgumentValueOrDefault ("--preview") == null) {
+			if (result.OptionResult ("--url") == null &&
+				result.OptionResult ("--stable") == null &&
+				result.OptionResult ("--preview") == null) {
 				return "At least one of --url, --stable, or --preview must be used";
 			}
 			return "";

Boots.Tests/Boots.Tests.csproj
@@ -18,6 +18,7 @@
 
   <ItemGroup>
     <ProjectReference Include="..\Boots.Core\Boots.Core.csproj" />
+    <ProjectReference Include="..\Boots\Boots.csproj" />
   </ItemGroup>
 
 </Project>

Boots.Tests/MainTests.cs
@@ -1,0 +1,49 @@
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Boots.Tests
+{
+	public class MainTests : IDisposable
+	{
+		const string DefaultErrorMessage = "At least one of --url, --stable, or --preview must be used";
+		readonly TextWriter consoleError;
+		readonly StringWriter stderr;
+		readonly MethodInfo main;
+
+		public MainTests ()
+		{
+			consoleError = Console.Error;
+			Console.SetError (stderr = new StringWriter ());
+
+			var program = Type.GetType ("Boots.Program, Boots", throwOnError: true);
+			main = program.GetMethod ("Main", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+			Assert.NotNull (main);
+		}
+
+		public void Dispose ()
+		{
+			Console.SetError (consoleError);
+		}
+
+		[Fact]
+		public async Task Empty ()
+		{
+			var args = Array.Empty<string> ();
+			var task = (Task) main.Invoke (null, new object [] { args });
+			await task;
+			Assert.Contains (DefaultErrorMessage, stderr.ToString ());
+		}
+
+		[Fact]
+		public async Task Stable ()
+		{
+			var args = new [] { "--stable", "Xamarin.Android" };
+			var task = (Task) main.Invoke (null, new object [] { args });
+			await task;
+			Assert.DoesNotContain (DefaultErrorMessage, stderr.ToString ());
+		}
+	}
+}