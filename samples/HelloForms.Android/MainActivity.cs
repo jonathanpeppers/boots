ff48a1f8c7191f6e841fafc9253900297dae674b
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-01-30 00:02:17 -0600

    Update sample Xamarin.Forms app (#65)
    * Update NuGet packages
    * `$(TargetFrameworkVersion)` of `v11.0`
    * Can delete Tabbar/Toolbar

--- samples/HelloForms.Android/MainActivity.cs
+++ samples/HelloForms.Android/MainActivity.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 using Android.App;
 using Android.Content.PM;
@@ -14,15 +14,13 @@ namespace HelloForms.Droid
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
-
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
