2b4c6e9274b97d871833056adf34f41b60a40c5d
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-06-03 10:00:18 -0500

    Update appcenter-pre-build.sh

--- samples/HelloForms.Android/appcenter-pre-build.sh
+++ samples/HelloForms.Android/appcenter-pre-build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # App Center custom build scripts: https://aka.ms/docs/build/custom/scripts
 
+printenv
+
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
 
 dotnet tool install --global boots --version 1.0.2.421
