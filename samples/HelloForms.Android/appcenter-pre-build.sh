46908884449f7f3c0d77ef4f572715b79aa4bee0
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-06-03 08:26:29 -0500

    [appcenter] set $JI_JAVA_HOME
    Fix for usage of JDK 14:
    
        error XA0030: Building with JDK version `14.0.2` is not supported. Please install JDK version `11.0`. See https://aka.ms/xamarin/jdk9-errors

--- samples/HelloForms.Android/appcenter-pre-build.sh
+++ samples/HelloForms.Android/appcenter-pre-build.sh
@@ -2,6 +2,7 @@
 # App Center custom build scripts: https://aka.ms/docs/build/custom/scripts
 
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
+export JI_JAVA_HOME="$JAVA_HOME_11_X64"
 
 dotnet tool install --global boots --version 1.0.2.421
 boots --preview Mono
