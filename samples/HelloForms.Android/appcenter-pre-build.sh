9ecb3bfc2702256f770ba5d39bca3c139726f1de
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-06-03 09:55:50 -0500

    Update appcenter-pre-build.sh

--- samples/HelloForms.Android/appcenter-pre-build.sh
+++ samples/HelloForms.Android/appcenter-pre-build.sh
@@ -2,9 +2,11 @@
 # App Center custom build scripts: https://aka.ms/docs/build/custom/scripts
 
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
-export JI_JAVA_HOME="$JAVA_HOME_11_X64"
 
 dotnet tool install --global boots --version 1.0.2.421
 boots --preview Mono
-boots --preview XamarinAndroid
-boots --preview XamariniOS
+boots --preview Xamarin.Android
+boots --preview Xamarin.iOS
+
+# set $JI_JAVA_HOME to point to a JDK 11
+echo '##vso[task.setvariable variable=JI_JAVA_HOME]$(JAVA_HOME_11_X64)'
